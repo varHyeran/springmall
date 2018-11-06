b0ce114741c13f3385a1a9677e497e5ff69ebd98
Administrator <Administrator@MSDN-SPECIAL>
2018-11-06 11:18:18 +0900

    [박혜란] spring boot

--- springmall/src/main/java/com/example/springmall/HiController.java
+++ springmall/src/main/java/com/example/springmall/HiController.java
@@ -2,13 +2,12 @@ package com.example.springmall;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@Controller	// 뷰가 없어도 사용할 수 있도록
+@Controller	// Controller인 애들을 모두 서블릿으로
 public class HiController {
 	@RequestMapping("/hi")	// hi라는 요청이 들어오면
 	public String hi() {
 		System.out.println("Hi Spring boot!");
-		return "hi";	// forward -> WEB-INF/jsp/hi.jsp
+		return "hi";	// forward -> /WEB-INF/jsp/hi.jsp
 	}
 }
---
+++ springmall/src/main/java/com/example/springmall/MemberService.java
@@ -0,0 +1,14 @@
+package com.example.springmall;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+	@Autowired
+	private MemberMapper memberMapper;
+	public int getCountMember() {
+		return 777;
+	//	return memberMapper.selectCountMember();
+	}
+}
