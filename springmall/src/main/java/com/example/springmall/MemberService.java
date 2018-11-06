7e426392c47a9c15b2caed1f574c65910b7463b4
Administrator <Administrator@MSDN-SPECIAL>
2018-11-06 14:51:43 +0900

    [박혜란] DB TEST
    Mapper -> (Repository) -> Service -> Controller -> JSP_VIEW

--- springmall/src/main/java/com/example/springmall/MemberService.java
+++ springmall/src/main/java/com/example/springmall/MemberService.java
@@ -2,13 +2,14 @@ package com.example.springmall;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional // 서비스 아래에만 붙을 수 있음. 서비스 안의 메소드가 실행이 안되면 전부 다 취소
 public class MemberService {
 	@Autowired
 	private MemberMapper memberMapper;
 	public int getCountMember() {
-		return 777;
-	//	return memberMapper.selectCountMember();
+		return memberMapper.selectCountMember();
 	}
 }
